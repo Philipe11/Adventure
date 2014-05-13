6a7c076e62b08539229de0a971c62b36af9018b1
Philipe Castilho <cpcastilho11@outlook.com>
2014-05-13 14:01:52 -0500

    Initial Commit

---
+++ Adventure/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adventure
+//
+//  Created by Philipe Castilho on 5/13/14.
+//  Copyright (c) 2014 Philipe's Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Adventure/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Adventure
+//
+//  Created by Philipe Castilho on 5/13/14.
+//  Copyright (c) 2014 Philipe's Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
